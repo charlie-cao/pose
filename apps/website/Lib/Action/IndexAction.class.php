567801ca09192cdef61e384875936c1b16a19dfd
charlie19817 <charlie19817@gmail.com>
2012-09-28 11:07:18 +0800

    login
    Change-Id: I5aa91a9d80ecd45a33e9e3eae009500ccf80880a

--- apps/website/Lib/Action/IndexAction.class.php
+++ apps/website/Lib/Action/IndexAction.class.php
@@ -1,16 +1,14 @@
 <?php
-
 class IndexAction extends Action {
-
-    public function index() {
-
-
-        $data = array();
-        $this->assign($data);
-        $this->display();
-    }
-
-   
-
+	public function index() {
+		$data = array ();
+		$this->assign ( $data );
+		$this->display ();
+	}
+	public function login($param) {
+		$data = array ();
+		$this->assign ( $data );
+		$this->display ();
+	}
 }
 
